55f52783d9a613d42e8c78edfef63c1af9b373d5
mertkaara <mertkara1995@gmail.com>
2023-11-07 21:53:28 +0300

    new file:   .vscode/settings.json 	new file:   output/temel_yapi.exe 	new file:   temel_program_yapisi.c

---
+++ temel_program_yapisi.c
@@ -0,0 +1,24 @@
+/* BTK Akademi C Programlama Dili kursu*/
+// Yorum satırı..
+
+#include <stdio.h>  // Ön işlemci direktifleri/preprocessor
+
+int main()          // Ana program
+{
+    printf("Hello, world!");    //Yazdırma fonksiyonu
+    return 0;
+}
+
+/*
+Geçersiz değişken isinleri      Nedeni
+$kazanc                         Değişken isminde $ işareti kullanılamaz.
+2boyutlu_tablo                  Değişken isminde ilk karakter rakam olamaz.
+toplam-değer                    Değişken isminde '-' işareti kullanılamaz.
+ogrenci no                      Değişken isminde boşluk karakteri kullanılmaz.
+int                             Anahtar sözcük, değişken ismi olarak kullanılamaz.
+*/
+
+/*
+&& ve
+|| veya
+*/
